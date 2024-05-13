69f14a66360f5dc11b51ebbb9c20e93fedb7b311
Shourav Chowdhury <chyshourav2@gmail.com>
2024-05-14 02:32:45 +0600

    migration issue

--- db/migrate/20240513192019_add_user_id_to_book.rb
+++ db/migrate/20240513192019_add_user_id_to_book.rb
@@ -1,5 +1,5 @@
 class AddUserIdToBook < ActiveRecord::Migration[7.1]
   def change
-    add_reference :books, :user, null: false, foreign_key: true
+    add_reference :books, :user, null: false,default: 0, foreign_key: true
   end
 end
